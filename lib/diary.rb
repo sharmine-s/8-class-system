b47b8b60591c3a700e24ca5ca1dbf2944e11f313
sharmine-s <sharmine.ss@gmail.com>
2023-04-14 17:05:16 +0100

    completed find best entry for reading time

--- lib/diary.rb
+++ lib/diary.rb
@@ -35,12 +35,18 @@ class Diary
   end
 
   def find_best_entry_for_reading_time(wpm, minutes)
-        # `wpm` is an integer representing the number of words the user can read
-        # per minute.
-        # `minutes` is an integer representing the number of minutes the user
-        # has to read.
-    # Returns an instance of diary entry representing the entry that is closest 
-    # to, but not over, the length that the user could read in the minutes they
-    # have available given their reading speed.
+    words = wpm * minutes
+    returns = @diary_entries[0]
+    difference = words
+    @diary_entries.each do |entry|
+      if entry.count_words <= words
+        if (words - entry.count_words) < difference
+          returns = entry
+          difference = words - entry.count_words          
+        end
+      end
+    end
+    returns
   end
 end
+
--- spec/diary_integration_spec.rb
+++ spec/diary_integration_spec.rb
@@ -46,5 +46,28 @@ RSpec.describe "Diary Integration" do
     expect(result).to eq 4
   end
 
+  it "finding best entry for reading time" do
+    diary_entry = DiaryEntry.new("Title_1", "word " * 100)
+    second_diary_entry = DiaryEntry.new("Title_2", "word " * 200)
+    diary = Diary.new
+    diary.add(diary_entry)
+    diary.add(second_diary_entry)
+    result = diary.find_best_entry_for_reading_time(100, 1)
+    expect(result).to eq diary_entry
+  end
+
+  it "finding best entry for reading time with three entries" do
+    diary_entry = DiaryEntry.new("Title_1", "first " * 90)
+    second_diary_entry = DiaryEntry.new("Title_2", "second " * 95)
+    third_diary_entry = DiaryEntry.new("Title_3", "third " * 101)
+    diary = Diary.new
+    diary.add(diary_entry)
+    diary.add(second_diary_entry)
+    diary.add(third_diary_entry)
+    result = diary.find_best_entry_for_reading_time(100, 1)
+    expect(result).to eq second_diary_entry
+  end
+
+  # need to test for when there are no diary entries
 
 end
